111fa2d4c7d73d21fda9f705dcdf198b7d2a689a
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2025-07-02 21:28:53 +0200

    feat: Add librarian class methods

--- src/com/library/model/Librarian.java
+++ src/com/library/model/Librarian.java
@@ -1,4 +1,33 @@
 package com.library.model;
 
-public class Librarian {
+/**
+ * Library staff member with higher borrow privileges.
+ */
+public class Librarian extends Person {
+    /**
+     * Constructs a Librarian.
+     * @param id    unique ID
+     * @param name  librarian name
+     * @param email contact email
+     * @param phone contact phone
+     */
+    public Librarian(String id, String name, String email, String phone) {
+        super(id, name, email, phone);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getMaxBorrowLimit() {
+        return 10;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void printInfo() {
+        System.out.printf("Librarian: %s (ID: %s)%n", name, id);
+    }
 }
