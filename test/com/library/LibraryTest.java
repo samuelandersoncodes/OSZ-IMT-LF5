9c1f9dacf1403e2faef63c9daff20536176571ec
samuelandersoncodes <samuelanderson@student.undip.ac.id>
2025-07-05 12:28:32 +0200

    test: Add librarytest basic structure

---
+++ test/com/library/LibraryTest.java
@@ -0,0 +1,10 @@
+package com.library;
+import com.library.model.*;
+import org.junit.jupiter.api.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LibraryTest {
+
+}
